6b923da18a23977bc97014090909b6960574faef
Ghost Liu <deluxghost@gmail.com>
2015-10-19 15:16:53 +0800

    turn off debug mode

--- run.py
+++ run.py
@@ -3,4 +3,4 @@
 from DelogX import app
 from DelogX import config
 
-app.run(host=config.app_info['HOST'], port=config.app_info['PORT'], debug=True)
+app.run(host=config.app_info['HOST'], port=config.app_info['PORT'])
