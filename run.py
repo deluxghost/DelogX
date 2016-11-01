d89cc5a9e20a469abd9639256f569beb79bca6ad
Ghost Liu <deluxghost@gmail.com>
2016-11-01 23:46:13 +0800

    updated start script

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from DelogX import app
-from DelogX import config
+from DelogX.config import DelogXConfig as config
 
 app.run(host=config.app_info['HOST'], port=config.app_info['PORT'])
